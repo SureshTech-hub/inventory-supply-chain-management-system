334b987210dda0e4835ceea3833ccd2bca53c72d
SureshTech-hub <nagireddysuresh630@gmail.com>
2026-06-16 20:36:57 +0530

    Initialize database on server startup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const db = require("./config/db");
+require("./database/initDB");
+
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const stockRoutes = require("./routes/stockRoutes");
